4f6b25536a7ffc8a1239345ee2ad96f42fcfce3a
azharsham <slicedspirit@gmail.com>
2021-06-06 21:47:29 +0530

    quiz version 1

--- dist/js/site.js
+++ dist/js/site.js
@@ -1,42 +1,105 @@
-
 let questions = [
   {
     id: 1,
-    question: "1.	How is a vision different from a dream? ?",
-    answer: "Random Access Memory",
-    options: [
-      "Random Access Memory",
-      "Randomely Access Memory",
-      "Run Aceapt Memory",
-      "None of these"
-    ]
+    question: "	You love making new friends all the time? ",
   },
   {
     id: 2,
-    question: "What is the full form of CPU?",
-    answer: "Central Processing Unit",
-    options: [
-      "Central Program Unit",
-      "Central Processing Unit",
-      "Central Preload Unit",
-      "None of these"
-    ]
+    question: "	You are more of a player on the ground rather than a spectator? ",
+
   },
   {
     id: 3,
-    question: "What is the full form of E-mail",
-    answer: "Electronic Mail",
-    options: [
-      "Electronic Mail",
-      "Electric Mail",
-      "Engine Mail",
-      "None of these"
-    ]
+    question: "	You are a livewire who cannot sit idle doing nothing most of the time? ",
+
+  },
+  {
+    id: 4,
+    question: "	You are always in search of new ideas and solutions to problems ",
+
+  },
+  {
+    id: 5,
+    question: "	Theories, lectures and philosophical reflections cannot hold your attention for long  ",
+
+  },
+  {
+    id: 6,
+    question: " You are a flexible and adaptable person ",
+  },
+  {
+    id: 7,
+    question: " You have an excellent innate talent to persuade people",
+
+  },
+  {
+    id: 8,
+    question: " You are largely a self-driven and self-motivated person who do not need anyone to oversee  you for any work/project you take up  ",
+
+  },
+  {
+    id: 9,
+    question: "	You are a keen observer of people and sensitive to their body language and minute shifts in their behaviour ",
+
+  },
+  {
+    id: 10,
+    question: "	You have an insatiable passion for the project you take up, no matter what, with exceptional tenacity and resilience to see it through ",
+
+  },
+  {
+    id: 11,
+    question: "	You are always brimming with new ideas and thoughts  ",
+  },
+  {
+    id: 12,
+    question: "You have a high sense of self-confidence and self-esteem  ",
+
+  },
+  {
+    id: 13,
+    question: "You always feel that there is a way to do things better ",
+
+  },
+  {
+    id: 14,
+    question: "	You can work alone or in a crowd or with a team if need be ",
+
+  },
+  {
+    id: 15,
+    question: "	Risks and challenges excite you  ",
+
+  },
+  {
+    id: 16,
+    question: " You love competition, run toward it and embrace it",
+  },
+  {
+    id: 17,
+    question: " You feel secure as an ideator, activator and getting your team run with you",
+
+  },
+  {
+    id: 18,
+    question: " You love most subjects and things under the Sun ",
+
+  },
+  {
+    id: 19,
+    question: " You are inclined to change the status quo and ways of doing things ",
+
+  },
+  {
+    id: 20,
+    question: "	You love innovating more than executing and managing organizational risks ",
+
   }
 ];
 
 let question_count = 0;
 let points = 0;
+let vals = 'Test';
 
 window.onload = function() {
   show(question_count);
@@ -54,19 +117,51 @@ function next() {
 
   let user_answer = document.querySelector("li.option.active").innerHTML;
   // check if the answer is right or wrong
-  if (user_answer == questions[question_count].answer) {
-    points += 10;
+  if (user_answer == "Most Likely") {
+    points += 2;
     sessionStorage.setItem("points", points);
   }
-  console.log(points);
+  if (user_answer == "Likely") {
+    points += 1;
+    sessionStorage.setItem("points", points);
+  }
+  if (user_answer == "Not Likely") {
+    points += -1;
+    sessionStorage.setItem("points", points);
+  }
+  if (user_answer == "Never") {
+    points += -2;
+    sessionStorage.setItem("points", points);
+  }
 
+  if(points > 10)
+  {
+  vals = "Entraprenuer";
+  sessionStorage.setItem("vals", vals);
+  }
+  else
+  {
+    vals = "Intraprenuer";
+    sessionStorage.setItem("vals", vals);
+  }
+  // if (user_answer == questions[question_count].answer) {
+  //   points += 10;
+  //   sessionStorage.setItem("points", points);
+  // }
+  console.log(points);
+ console.log(vals)
   question_count++;
   show(question_count);
 }
 
 function show(count) {
   let question = document.getElementById("questions");
-  let [first, second, third, fourth] = questions[count].options;
+  let [first, second, third, fourth] = [
+    "Most Likely",
+    "Likely",
+    "Not Likely",
+    "Never"
+  ]
 
   question.innerHTML = `
   <h2>Q${count + 1}. ${questions[count].question}</h2>
--- dist/js/userInfo.js
+++ dist/js/userInfo.js
@@ -1,7 +1,9 @@
 let user_name = sessionStorage.getItem("name");
 let user_points = sessionStorage.getItem("points");
 let user_time = sessionStorage.getItem("time");
+let user_vals = sessionStorage.getItem("vals");
 document.querySelector("span.name").innerHTML = user_name;
+document.querySelector("span.vals").innerHTML = user_vals;
 document.querySelector("span.points").innerHTML = user_points;
-document.querySelector("span.time_taken").innerHTML = user_time;
+// document.querySelector("span.time_taken").innerHTML = user_time;
 
